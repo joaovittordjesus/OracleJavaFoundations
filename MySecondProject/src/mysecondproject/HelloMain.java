1d483c3dd46ae514cc6aba5d948efb2dbb5cd98d
joaovittordjesus <105802915+joaovittordjesus@users.noreply.github.com>
2023-08-08 14:41:44 -0300

    Add files via upload

---
+++ MySecondProject/src/mysecondproject/HelloMain.java
@@ -0,0 +1,11 @@
+package mysecondproject;
+
+public class HelloMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello again world");
+
+	}
+
+}
