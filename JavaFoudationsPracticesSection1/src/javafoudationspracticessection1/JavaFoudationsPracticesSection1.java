6868afb9bed619edd297ccc3f243136f3758ed8d
joaovittordjesus <joaovittordomingosdejesus@outlook.com>
2023-10-18 13:45:53 -0300

    Organizing the repository information

---
+++ JavaFoudationsPracticesSection1/src/javafoudationspracticessection1/JavaFoudationsPracticesSection1.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package javafoudationspracticessection1;
+
+/**
+ *
+ * @author DevTardeA
+ */
+public class JavaFoudationsPracticesSection1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hello what a wonderful world!!!");
+        System.out.println("Java Foundations Practices Section 1");
+        
+                
+             
+    }
+    
+}
